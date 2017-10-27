e1c43b11355821911cd6fad409f9bbc4a554d128
Zhehui Zhou <grayy921013@gmail.com>
2017-10-27 14:35:54 -0700

    Add script for capturing sensor value

---
+++ capture_data.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python3
+import serial
+import os
+from datetime import datetime
+import threading
+
+
+ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+name = input("Enter the character: ")
+if len(name) != 1:
+    print("wrong length")
+    exit()
+char = name.upper()
+if not os.path.exists(char):
+    os.makedirs(char)
+
+
+class WorkerThread(threading.Thread):
+    def __init__(self):
+        super(WorkerThread, self).__init__()
+        self.f = None
+        self.quit = False
+
+    def run(self):
+        while not self.quit:
+            data = ser.readline().decode("utf-8")
+            if self.f:
+                f.write(data)
+
+    def pause(self):
+        self.f = None
+
+    def kill(self):
+        self.quit = True
+
+i = 0
+t = WorkerThread()
+t.start()
+
+while i < 10:
+    input("#" + str(i) + " ready? ")
+    f = open(char + "/" + str(datetime.now()), "w+")
+    t.f = f
+    input("finish?")
+    print("Goodbye!")
+    t.pause()
+    i += 1
+t.kill()
