7960cefcfa7c9c1990cfdd5d3db359cbed754413
tsushen <ztsushen@gmail.com>
2017-11-16 14:06:54 -0800

    update online inference

--- eval.py
+++ eval.py
@@ -2,11 +2,57 @@ import config
 import data
 from keras.models import load_model
 import numpy as np
+import serial
+import threading
 
 model_name = 'model_default.model'
 
-x_test = data.get_from_test_folder()
+ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+array = []
 
-model = load_model(model_name)
-res = model.predict_classes(x_test[0], batch_size=config.batch_size, verbose=0)
-print (res[0])
+class WorkerThread(threading.Thread):
+    def __init__(self, x):
+        super(WorkerThread, self).__init__()
+        self.collecting = False
+        self.quit = False
+        self.x = x
+        self.lock = threading.Lock()
+
+    def run(self):
+        while True:
+            data = ser.readline().decode("utf-8").strip().split(",")
+            with self.lock:
+                if self.collecting:
+                    int_data = []
+                    for p in data:
+                        int_data.append(int(p))
+                    self.x.append(int_data)
+                if self.quit:
+                    return
+
+    def collect(self, should_collect):
+        with self.lock:
+            self.collecting = should_collect
+
+    def kill(self):
+        with self.lock:
+            self.quit = True
+
+    def clear(self):
+        with self.lock:
+            del self.x[:]
+
+t = WorkerThread(array)
+t.start()
+
+while True:
+    input(" ready? ")
+    t.collect(True)
+    input("finish?")
+    print(array)
+    t.collect(False)
+    t.clear()
+
+# model = load_model(model_name)
+# res = model.predict_classes(x_test[0], batch_size=config.batch_size, verbose=0)
+# print (res[0])
